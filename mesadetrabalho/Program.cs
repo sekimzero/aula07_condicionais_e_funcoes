06699a69039e1c292819e5d129f606987a60fcd9
Marcelo alves <marcelloalves09@gmail.com>
2022-06-09 18:06:17 -0300

    mesa de trabalho

---
+++ mesadetrabalho/Program.cs
@@ -0,0 +1,34 @@
+﻿// Cotação do dólar no dia 3 de maio de 2022.
+double cotacaoDoDolarHoje = 4.97;
+const double VALOR_MINIMO_DE_COMPRA_EM_DOLAR = 100;
+const double VALOR_MAXIMO_DE_COMPRA_EM_DOLAR = 2500;
+
+Console.WriteLine("Digite o valor em Real: ");
+double.TryParse(Console.ReadLine(), out double valorDaCompraEmReais);
+
+string converterRealParaDolar(double valorDaCompraEmReais)
+{
+
+    double valorDisponivelEmDolar = valorDaCompraEmReais / cotacaoDoDolarHoje;
+    string result;
+    if(VALOR_MINIMO_DE_COMPRA_EM_DOLAR > valorDisponivelEmDolar)
+    {
+        result =  "A compra mínima permitida é de $" + VALOR_MINIMO_DE_COMPRA_EM_DOLAR;
+    }
+            
+    else if(VALOR_MAXIMO_DE_COMPRA_EM_DOLAR < valorDisponivelEmDolar)
+    {
+        result = "A compra máxima permitida é de $" + VALOR_MAXIMO_DE_COMPRA_EM_DOLAR;
+
+    }
+    else
+    {
+        result = "Você pode comprar $" + valorDisponivelEmDolar;
+    }
+    return result;
+    
+}
+Console.WriteLine(converterRealParaDolar(valorDaCompraEmReais));
+
+
+
