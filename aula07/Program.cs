230ae1a6d2edafd292828033d93a98b95a2bbc70
salomao kim <sekim.maua@gmail.com>
2022-06-11 11:51:05 -0300

    Criado a Solucao para a aula 7 "Cotacao do Dolar"

--- aula07/Program.cs
+++ aula07/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Cotação do dólar no dia 3 de maio de 2022.
+
+
+// Utilize as mensagens a seguir como base do retorno esperado:
+// A compra mínima permitida é de $ XXX,XX.
+// A compra máxima permitida é de $ XXXX,XX.
+// Você pode comprar $ XXX,XX.
+
+double cotacaoDoDolarHoje = 4.97;
+const double VALOR_MINIMO_DE_COMPRA_EM_DOLAR = 100;
+const double VALOR_MAXIMO_DE_COMPRA_EM_DOLAR = 2500;
+double valorDaCompraEmReais;
+
+
+if (valorDaCompraEmReais>100)
+{
+string converterRealParaDolar(valorDaCompraEmReais) {
+}
+
+
+}
+
+
+
+
+
+
+
+Console.WriteLine(converterRealParaDolar());
